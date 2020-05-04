8c95d2ef907cb22bf3c5e1339624dcb84ba242ca
Tim McGrath <tim@wanderfeast.com>
2020-05-04 11:23:48 -0700

    Checkin progress on URL update
    Checkin progress on URL update

---
+++ Kevinsuggest1.py
@@ -0,0 +1,8 @@
+
+{
+    "filepath1": {
+        "old1": "new1",
+        "old2": "new2"
+    },
+    "filepath2": {
+        ...
---
+++ replace-pdflink.py
@@ -0,0 +1,8 @@
+# Replace the links to internal PDF's with links that have been
+# updated to point to external pdf sources. Some links may need to be removed as
+# a secondary task.
+
+import os
+import csv
+
+#take
