22c70de8dfefce26aa75700f590d46c31e06add2
Tim McGrath <tim@wanderfeast.com>
2020-06-06 10:14:30 -0700

    Daily commit
    Something still not working with finding the identified href tag.

--- updateURL.py
+++ updateURL.py
@@ -10,28 +10,29 @@ import csv
 from bs4 import BeautifulSoup
 from bs4 import Comment
 
-def replaceURL(html_file, orig_url, replace_url):
-    # open the html file
-    f = open(html_file, mode='r', encoding = 'utf-8')
-    soup = BeautifulSoup(f, 'html.parser')
-    # Replacing Impairment Issues
-    target = soup.find("a", text= orig_url)
-    if target is not None:
-        mycomment =  Comment('Replacement comment for Impairment Issues')
-        replacement = target.replace_with(replace_url)
-    prettyHTML = soup.prettify()
-    of = open(html_file, mode='w', encoding='utf-8')
-    of.write(prettyHTML)
-    of.close()
-    f.close()
+count = 0
 
 # 1) open the csv with replacement URL's
 infile_path = '/Users/Tim/Code/Github/BMPTest/URLReplaceTest.csv'
 with open(infile_path) as rf:
     for line in rf:
-        elements = line.split()
-        html_file = elements[0]
-        orig_url = elements[1]
-        replace_url = elements[2]
-        replaceURL(html_file, orig_url, replace_url)
+        elements = line.split(',')
+        html_file = elements[0].strip('"')
+        orig_url = elements[1].strip('"')
+        replace_url = elements[2].strip()
+        # replace_url = replace_url.strip('"') # make sure all double quotes are stripped.
+        replace_url = replace_url.replace('"', "")
+        f = open(html_file, mode='r', encoding = 'utf-8')
+        soup = BeautifulSoup(f, 'html.parser')
+        f.close()
+        # Replacing URL
+        target = soup.find('a', href = orig_url)
+        if target is not None:
+            count += 1
+            print('replacing', count)
+            replacement = target.replace_with(replace_url)
+        prettyHTML = soup.prettify()
+        of = open(html_file, mode='w', encoding='utf-8')
+        of.write(prettyHTML)
+        of.close()
 rf.close()
