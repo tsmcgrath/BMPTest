8a276758d302405629b1e6b8ac5f68748549a895
Tim McGrath <tim@wanderfeast.com>
2020-10-23 15:36:41 -0700

    Interim checkin
    Interim checkin. Still not working.

---
+++ emptydivs_intel.py
@@ -0,0 +1,22 @@
+# python script to list the contents of sidebars on static html pages
+from bs4 import BeautifulSoup
+import os
+# The top argument for the directory walk
+topdir = '.'
+
+# The extension to search for
+exten = '.html'
+
+for dirpath, dirnames, files in os.walk(topdir):
+    for name in files:
+        if name.lower().endswith(exten):
+            # print(os.path.join(dirpath, name))
+            file_path = os.path.join(dirpath, name)
+            f = open(file_path, "r+")
+            soup = BeautifulSoup(f, 'html.parser')
+            # soupbar = soup.find("div", "sidebar-content")
+            # sbar_menu = soupbar.findall("li", {"class": "li-content text-body"})
+            # sbars = soup.find_all(text=re.compile('sidebar'))
+            mydivs = soup.find_all("div", class_= "post")
+            print(file_path, ',', mydivs.[0])
+
